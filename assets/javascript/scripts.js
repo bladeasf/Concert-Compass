7c4b8c40a181a3bd623a9730185462841033fc77
Evan Chang <evanchang17812@hotmail.com>
2023-11-09 21:01:26 -0500

    Added basic JS function shells

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,4 +1,37 @@
-var apikey1 = '';
-var apikey2 = '';
+var geniusapikey = '';
+var seatgeekapikey = '';
 
 
+function trendingartist(){
+    var apiurl = '';
+    var topartist = {
+        name1: '',
+        name2: '',
+        name3: ''
+    }
+}
+
+function upcomingconcerts(){
+    var apiurl = '';
+    var upcomingconcerts{
+        upcomingconcerts1 = '',
+        upcomingconcerts2 = '',
+        upcomingconcerts3 = ''
+    }
+}
+
+function artistinfo(){
+    var apiurl = ''
+    var artist = {
+        name : "",
+        lastestAlbum: "",
+        topsong: ""
+    }
+}
+
+function top100artists(){
+    var apiurl = ''
+    var artists = {
+        namestring: ''
+    }
+}
