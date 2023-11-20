075cabd1f4c5ffc7a9ce9e15a246aafb06c9fddc
Evan Chang <evanchang17812@hotmail.com>
2023-11-19 23:28:56 -0500

    added functionality to music and shows button

--- assets/javascript/artist.js
+++ assets/javascript/artist.js
@@ -1,9 +1,10 @@
-
+var musicbutton = document.getElementById("music-btn");
+var showsbutton = document.getElementById("shows-btn");
 
 populateSongList();
 
 function populateSongList(){
-    var storedArtist = localStorage.getItem('artist');
+var storedArtist = localStorage.getItem('artist');
 var storedSongs = JSON.parse(localStorage.getItem('songs'));
 var storedAvatar = localStorage.getItem('avatar');
 var storedAlbum = localStorage.getItem('album');
@@ -11,15 +12,51 @@ console.log("Stored Artist:", storedArtist);
 console.log("Stored Songs:", storedSongs);
 console.log("Stored Avatar:", storedAvatar);
 console.log("Stored Album:", storedAlbum);
-    var songcontainer = document.getElementById("songcontainer");
-    var ol = document.createElement('ol');
-    songcontainer.append(ol);
-    for (var i = 0; i < storedSongs.length; i++){
-        var songname = document.createElement('li');
-        songname.textContent = storedSongs[i];
-        ol.append(songname);
-    }
-    document.getElementById('artist-pic').src = storedAvatar;
+var songcontainer = document.getElementById("songcontainer");
+var ol = document.createElement('ol');
+ol.setAttribute('id', 'ol');
+songcontainer.append(ol);
+for (var i = 0; i < storedSongs.length; i++){
+    var songname = document.createElement('li');
+    songname.textContent = storedSongs[i];
+    ol.append(songname);
+}
+document.getElementById('artist-pic').src = storedAvatar;
 document.getElementById('album-img').src = storedAlbum;
 }
 
+
+
+function getMusic(){
+    window.location.replace("artist.html");
+}
+
+function removeMusic(){
+    var ol = document.getElementById("ol");
+    ol.remove();
+}
+
+function getShows(){
+var storedConcerts = JSON.parse(localStorage.getItem('concerts'));
+console.log("storedConcerts:", storedConcerts);
+
+var showscontainer = document.getElementById("songcontainer");
+var ol = document.createElement('ol');
+showscontainer.append(ol);
+
+for (var i = 0; i < storedConcerts.events.length; i++){
+    var concertinfo = document.createElement('li');
+    concertinfo.textContent = storedConcerts.events[i].title+ " " + storedConcerts.events[i].venue.name + " "+ storedConcerts.events[i].datetime_local;
+    ol.append(concertinfo);
+}
+}
+
+musicbutton.addEventListener("click", function () {
+    getMusic();
+})
+
+showsbutton.addEventListener("click", function () {
+    removeMusic();
+    getShows();
+
+})
